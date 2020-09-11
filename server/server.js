0dc8f5d16c0b59032b0d9cee7979e1f2b22ba73a
AlexanderLanderos <Alexander.Landeros1@gmail.com>
2020-09-10 17:51:40 -0700

    everything is working back end setup

--- server/server.js
+++ server/server.js
@@ -4,16 +4,28 @@ const path = require('path');
 const port = 3000;
 const app = express();
 
+const client = require('../index.js');
+
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../index.html'));
+  console.log('You are at the login page');
+});
 
 app.get('/styles.css', (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../styles.css'));
   console.log('Get request received for CSS');
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../index.html'));
-  console.log('');
+app.get('/signup', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../signup.html'));
+  console.log('You are at the signup page!');
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../login.html'));
+  console.log('You are at the login page!');
 });
 
 //CRUD routing goes here
