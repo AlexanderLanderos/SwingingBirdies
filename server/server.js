14b63ad71b7ea72ee075e99e23207974a6fe7564
Alexander Landeros <AREX@AREX-MBP.charter.com>
2020-12-06 22:35:25 -0800

    removed all un-needed routes

--- server/server.js
+++ server/server.js
@@ -8,15 +8,13 @@ app.use(express.json());
 
 app.get('/styles.css', (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../styles.css'));
-  console.log('Get request received for CSS');
 });
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../index.html'));
-  console.log('');
 });
 
-//CRUD routing goes here
+
 
 // Make sure my server is listening on the correct port
 app.listen(port, () => {
