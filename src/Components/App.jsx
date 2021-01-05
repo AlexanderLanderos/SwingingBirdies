2a02ce974ad94c8fbf0a8f3d5459325b3b8c7bd6
Alexander Landeros <AREX@AREX-MBP.charter.com>
2021-01-04 22:46:50 -0800

    added gitignore

---
+++ src/Components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.html';
+
+const App = () => {
+  render(){
+    return(
+    <div className='App'>
+      <header>
+        <h1>Swing Tracker</h1>
+      </header>
+      <div>
+        <h1>First</h1>
+      </div>
+    </div>
+  )
+  }
+  
+};
+export default App;
